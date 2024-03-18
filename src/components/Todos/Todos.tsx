44d29d5d173f3fa4e74bd4c7d1d9da90367f90ab
Holland Ho <ho.holland1999@gmail.com>
2024-03-17 17:48:42 -0700

    Added loading state spinner and updated css styling to match figma design

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, Form, Row } from "react-bootstrap";
+import { Card, Col, Form, Row, Spinner } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import "./Todos.css";
 import testData from "../../data/testData.json"
@@ -6,20 +6,13 @@ import testData from "../../data/testData.json"
 const Todos = () => {
   const currentDate = new Date();
 
-  const [todos, setTodos] = useState(testData);
+  const [todos, setTodos] = useState([]);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isLoading, setIsLoading] = useState(false);
 
 
   useEffect(() => {
     setIsLoading(true); // Set loading before sending API request
-    // TODO: Use fetch to handle GET API call to the backend for todo list data
-    // fetch('https://some.api.com/data')
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         setTodos(data);
-    //         setIsLoading(false);
-    //     });
     fetch('https://944ba3c5-94c3-4369-a9e6-a509d65912e2.mock.pstmn.io/get', {
         method: 'GET',
         headers: {
@@ -39,7 +32,9 @@ const Todos = () => {
     })
     .catch(error => {
       setIsLoading(false); // Stop loading in case of error  
-      console.error("error =========>",error)
+      console.error("error =========>", error)
+      console.log("Loading mock data instead")
+      setTodos(testData);
     });
   }, []);
 
@@ -53,6 +48,7 @@ const Todos = () => {
    * @param newComplete 
    */
   function updateCompletedTodo(todoId: string, newComplete: boolean) {
+    setIsLoading(true); // Set Loading before sending API request
     setTodos((prev) => {
       const newTodos = [...prev];
       const findTodoIndex = newTodos.findIndex((todo) => todo.id === todoId);
@@ -70,185 +66,210 @@ const Todos = () => {
       }
       throw new Error ('Something went wrong: ' + response.status + " Error")
     })
-    .then(data => console.log(data))
-    .catch(error => console.error("error =========>",error));
+    .then(data => {
+      setIsLoading(false);
+      console.log(data)})
+    .catch(error => {
+      setIsLoading(false);
+      console.error("error =========>",error)
+    });
     return newTodos;
     });
   }
 
   return (
-    <div className="todo-cards">
-      {/* Overdue items at the top */}
-      {todos
-        .filter(
-          (todo) =>
-            !todo.isComplete &&
-            todo.dueDate !== null &&
-            new Date(todo.dueDate) < currentDate
-        )
-        .sort((a, b) => {
-          if (a.dueDate === b.dueDate) {
+    <>
+      <div className="loading-state">
+        {isLoading ? 
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+            : null}
+      </div>
+      <div className="todo-cards">
+        {/* Overdue items at the top */}
+        {todos
+          .filter(
+            (todo) =>
+              !todo.isComplete &&
+              todo.dueDate !== null &&
+              new Date(todo.dueDate) < currentDate
+          )
+          .sort((a, b) => {
+            if (a.dueDate === b.dueDate) {
+              return 0;
+            }
+
+            if (a.dueDate === null) {
+              return 1;
+            }
+
+            if (b.dueDate === null) {
+              return -1;
+            }
+
+            const dueDateA = new Date(a.dueDate);
+            const dueDateB = new Date(b.dueDate);
+
+            if (dueDateA < dueDateB) {
+              return -1;
+            }
+
+            if (dueDateA > dueDateB) {
+              return 1;
+            }
+
             return 0;
-          }
+          })
+          .map((todo) => {
+            return (
+              <Card style={{ background: "#FF7F7F" }} className="mt-3">
+                <Row>
+                  <Col>
+                    <Card.Body>
+                      <Form.Check
+                        type="checkbox"
+                        key={todo.id}
+                        label={todo.description}
+                        checked={todo.isComplete}
+                        onChange={() =>
+                          updateCompletedTodo(todo.id, !todo.isComplete)
+                        }
+                      ></Form.Check>
+                    </Card.Body>
+                  </Col>
+                  <Col style={{display: 'flex', justifyContent:'right', paddingRight: '50px'}}>
+                    <div className="date-text">
+                      {new Date(todo.dueDate).toLocaleDateString()}
+                    </div>
+                    </Col>
+                </Row>
+              </Card>
+            );
+          })}
+        {/* Sort by dates by due soonest and no due dates  */}
+        {todos
+          .filter(
+            (todo) =>
+              !todo.isComplete &&
+              (todo.dueDate == null || new Date(todo.dueDate) > currentDate)
+          )
+          .sort((a, b) => {
+            if (a.dueDate === b.dueDate) {
+              return 0;
+            }
 
-          if (a.dueDate === null) {
-            return 1;
-          }
+            if (a.dueDate === null) {
+              return 1;
+            }
 
-          if (b.dueDate === null) {
-            return -1;
-          }
+            if (b.dueDate === null) {
+              return -1;
+            }
 
-          const dueDateA = new Date(a.dueDate);
-          const dueDateB = new Date(b.dueDate);
+            const dueDateA = new Date(a.dueDate);
+            const dueDateB = new Date(b.dueDate);
 
-          if (dueDateA < dueDateB) {
-            return -1;
-          }
+            if (dueDateA < dueDateB) {
+              return -1;
+            }
 
-          if (dueDateA > dueDateB) {
-            return 1;
-          }
+            if (dueDateA > dueDateB) {
+              return 1;
+            }
 
-          return 0;
-        })
-        .map((todo) => {
-          return (
-            <Card style={{ background: "#FF7F7F" }} className="mt-3">
-              <Row>
-                <Col>
-                  <Card.Body>
-                    <Form.Check
-                      type="checkbox"
-                      key={todo.id}
-                      label={todo.description}
-                      checked={todo.isComplete}
-                      onChange={() =>
-                        updateCompletedTodo(todo.id, !todo.isComplete)
-                      }
-                    ></Form.Check>
-                  </Card.Body>
-                </Col>
-                <Col>{new Date(todo.dueDate).toLocaleDateString()}</Col>
-              </Row>
-            </Card>
-          );
-        })}
-      {/* Sort by dates by due soonest and no due dates  */}
-      {todos
-        .filter(
-          (todo) =>
-            !todo.isComplete &&
-            (todo.dueDate == null || new Date(todo.dueDate) > currentDate)
-        )
-        .sort((a, b) => {
-          if (a.dueDate === b.dueDate) {
             return 0;
-          }
+          })
+          .map((todo) => {
+            return (
+              <Card className="mt-3">
+                <Row>
+                  <Col>
+                    <Card.Body>
+                      <Form.Check
+                        type="checkbox"
+                        key={todo.id}
+                        label={todo.description}
+                        checked={todo.isComplete}
+                        onChange={() =>
+                          updateCompletedTodo(todo.id, !todo.isComplete)
+                        }
+                      ></Form.Check>
+                    </Card.Body>
+                  </Col>
+                  <Col style={{display: 'flex', justifyContent:'right', paddingRight: '50px'}}>
+                    {todo.dueDate !== null
+                      ? 
+                      <div className="date-text">
+                        {new Date(todo.dueDate).toLocaleDateString()}
+                      </div>
+                      : null}
+                  </Col>
+                </Row>
+              </Card>
+            );
+          })}
+        {/* Completed items at the bottom */}
+        {todos
+          .filter((todo) => todo.isComplete)
+          .sort((a, b) => {
+            if (a.dueDate === b.dueDate) {
+              return 0;
+            }
 
-          if (a.dueDate === null) {
-            return 1;
-          }
+            if (a.dueDate === null) {
+              return 1;
+            }
 
-          if (b.dueDate === null) {
-            return -1;
-          }
+            if (b.dueDate === null) {
+              return -1;
+            }
 
-          const dueDateA = new Date(a.dueDate);
-          const dueDateB = new Date(b.dueDate);
+            const dueDateA = new Date(a.dueDate);
+            const dueDateB = new Date(b.dueDate);
 
-          if (dueDateA < dueDateB) {
-            return -1;
-          }
+            if (dueDateA < dueDateB) {
+              return -1;
+            }
 
-          if (dueDateA > dueDateB) {
-            return 1;
-          }
+            if (dueDateA > dueDateB) {
+              return 1;
+            }
 
-          return 0;
-        })
-        .map((todo) => {
-          return (
-            <Card className="mt-3">
-              <Row>
-                <Col>
-                  <Card.Body>
-                    <Form.Check
-                      type="checkbox"
-                      key={todo.id}
-                      label={todo.description}
-                      checked={todo.isComplete}
-                      onChange={() =>
-                        updateCompletedTodo(todo.id, !todo.isComplete)
-                      }
-                    ></Form.Check>
-                  </Card.Body>
-                </Col>
-                <Col>
-                  {todo.dueDate !== null
-                    ? new Date(todo.dueDate).toLocaleDateString()
-                    : null}
-                </Col>
-              </Row>
-            </Card>
-          );
-        })}
-      {/* Completed items at the bottom */}
-      {todos
-        .filter((todo) => todo.isComplete)
-        .sort((a, b) => {
-          if (a.dueDate === b.dueDate) {
             return 0;
-          }
+          })
+          .map((todo) => {
+            return (
+              <Card style={{ background: "#90ee90" }} className="mt-3">
+                <Row>
+                  <Col>
+                    <Card.Body>
+                      <Form.Check
+                        type="checkbox"
+                        key={todo.id}
+                        label={todo.description}
+                        checked={todo.isComplete}
+                        onChange={() =>
+                          updateCompletedTodo(todo.id, !todo.isComplete)
+                        }
+                      ></Form.Check>
+                    </Card.Body>
+                  </Col>
+                  <Col style={{display: 'flex', justifyContent:'right', paddingRight: '50px'}}>
+                      {todo.dueDate !== null
+                        ? 
+                        <div className="date-text">
+                        {new Date(todo.dueDate).toLocaleDateString()}
+                        </div>
+                        : null}
+                  </Col>
+                </Row>
+              </Card>
+            );
+          })}
+      </div>
+    </>
 
-          if (a.dueDate === null) {
-            return 1;
-          }
-
-          if (b.dueDate === null) {
-            return -1;
-          }
-
-          const dueDateA = new Date(a.dueDate);
-          const dueDateB = new Date(b.dueDate);
-
-          if (dueDateA < dueDateB) {
-            return -1;
-          }
-
-          if (dueDateA > dueDateB) {
-            return 1;
-          }
-
-          return 0;
-        })
-        .map((todo) => {
-          return (
-            <Card style={{ background: "#90ee90" }} className="mt-3">
-              <Row>
-                <Col>
-                  <Card.Body>
-                    <Form.Check
-                      type="checkbox"
-                      key={todo.id}
-                      label={todo.description}
-                      checked={todo.isComplete}
-                      onChange={() =>
-                        updateCompletedTodo(todo.id, !todo.isComplete)
-                      }
-                    ></Form.Check>
-                  </Card.Body>
-                </Col>
-                <Col>
-                  {todo.dueDate !== null
-                    ? new Date(todo.dueDate).toLocaleDateString()
-                    : null}
-                </Col>
-              </Row>
-            </Card>
-          );
-        })}
-    </div>
   );
 };
 
